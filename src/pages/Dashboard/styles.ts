0c85cd533b67ea6e3fd708608f19784f2da599c3
jfilipe-dev <jfilipe1511@gmail.com>
2020-06-13 17:36:24 -0300

    Added dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+
 import colors from '../../styles/colors';
 
 export const Container = styled.div``;
@@ -52,5 +54,210 @@ export const Profile = styled.div`
     strong {
       color: ${colors.color2};
     }
+  }
+`;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    margin-top: 8px;
+    color: ${colors.color2};
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+      width: 1.3px;
+      height: 12px;
+      background: ${colors.color2};
+      margin: 0 8px;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: ${colors.color4};
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    background: ${colors.color3};
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 24px;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 80%;
+      width: 2px;
+      left: 0;
+      top: 10%;
+      background: ${colors.color2};
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: ${colors.color1};
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: ${colors.color4};
+
+      svg {
+        color: ${colors.color2};
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Section = styled.section`
+  margin-top: 48px;
+
+  > strong {
+    color: ${colors.color4};
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid ${colors.color4};
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+
+  > p {
+    color: ${colors.color4};
+  }
+`;
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 16px;
+  }
+
+  span {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    color: ${colors.color4};
+    width: 70px;
+
+    svg {
+      color: ${colors.color2};
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    flex: 1;
+    background: ${colors.color3};
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-left: 24px;
+
+    img {
+      width: 56px;
+      height: 56px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: ${colors.color1};
+      font-size: 20px;
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+
+  .DayPicker {
+    background: ${colors.color3};
+    border-radius: 10px;
+  }
+
+  .DayPicker-wrapper {
+    padding-bottom: 0;
+  }
+
+  .DayPicker,
+  .DayPicker-Month {
+    width: 100%;
+  }
+
+  .DayPicker-Month {
+    border-collapse: separate;
+    border-spacing: 8px;
+    margin: 16px;
+  }
+
+  .DayPicker-Day {
+    width: 40px;
+    height: 40px;
+  }
+
+  .DayPicker-Day--available:not(.DayPicker-Day--outside) {
+    background: ${colors.background};
+    border-radius: 10px;
+    color: ${colors.color1};
+  }
+
+  .DayPicker:not(.DayPicker--interactionDisabled)
+    .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+    background: ${shade(0.2, `${colors.color4}`)};
+  }
+
+  .DayPicker-Day--today {
+    font-weight: normal;
+  }
+
+  .DayPicker-Day--disabled {
+    color: ${colors.color4} !important;
+    background: transparent !important;
+  }
+
+  .DayPicker-Day--selected {
+    background: ${colors.color2} !important;
+    border-radius: 10px;
+    color: #232129 !important;
   }
 `;
